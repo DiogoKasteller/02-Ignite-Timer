ae4de1a6e72fd291b85c9d31ab68e375566e96ff
Diogo Kasteller <diogokasteller@gmail.com>
2024-09-24 14:41:46 -0300

    adding button styled component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import { Button } from "./components/Button"
+
 export function App() {
-  return <h1></h1>
+  return (
+    <>
+      <Button variant="primary"></Button>
+      <Button variant="secondary"></Button>
+      <Button variant="danger"></Button>
+      <Button variant="success"></Button>
+      <Button></Button>
+    </>
+  )
 }

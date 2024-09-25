2065dc42a1ea5a3c37344a0a5873097081db1d6d
Diogo Kasteller <diogokasteller@gmail.com>
2024-09-24 21:30:15 -0300

    adding Router and Default Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import { ThemeProvider } from "styled-components"
-import { Button } from "./components/Button"
 import { GlobalStyle } from "./styles/global"
 import { defaultTheme } from "./styles/themes/default"
+import { Router } from "./Router"
+import { BrowserRouter } from "react-router-dom"
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary"></Button>
-      <Button variant="secondary"></Button>
-      <Button variant="danger"></Button>
-      <Button variant="success"></Button>
-      <Button></Button>
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle></GlobalStyle>
     </ThemeProvider>
   )

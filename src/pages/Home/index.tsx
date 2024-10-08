e2f160c6391d7cf66453df99ad90f869a4cf2757
Diogo Kasteller <diogokasteller@gmail.com>
2024-10-08 15:03:56 -0300

    finishing countdown cycle

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -20,7 +20,7 @@ const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, "Informe a tarefa"),
   minutesAmount: zod
     .number()
-    .min(1, "O ciclo precisa ser de no mínimo 5 minutos.")
+    .min(5, "O ciclo precisa ser de no mínimo 5 minutos.")
     .max(60, "O ciclo precisa ser de no máximo 60 minutos."),
 })
 
@@ -160,8 +160,8 @@ export function Home() {
             id="minutesAmount"
             type="number"
             placeholder="00"
-            step={1}
-            min={1}
+            step={5}
+            min={5}
             max={60}
             disabled={!!activeCycle}
             {...register("minutesAmount", { valueAsNumber: true })}
